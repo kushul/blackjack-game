1a3811ac8840b344ebc9563d07cc5f2d6759bc6d
Kushul Soomaree <kushul@lsl.digital>
2024-02-26 18:39:20 +0400

    setting up styles

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 let firstCard = 10
 let secondCard = 7
 let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
+let message = "";
+let messageEl = document.getElementById("message-el")
 
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
-
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're out of the game! 😭")
+const startGame = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
+
