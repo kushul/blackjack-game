e72a15be6cad2ac5e7b23ef35d3bd3ff7d6ce8e3
Kushul Soomaree <kushul@lsl.digital>
2024-02-26 23:02:00 +0400

    render logic

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard + 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = "";
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+    return 5;
+}
 
 const startGame = () => {
+    renderGame()
+}
+
+const renderGame = () => {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += `${cards[i]} `
+    }
+    sumEl.textContent = `Sum: ${sum}`
     if (sum <= 20) {
         message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
@@ -19,3 +35,9 @@ const startGame = () => {
     messageEl.textContent = message
 }
 
+const newCard = () => {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+}
